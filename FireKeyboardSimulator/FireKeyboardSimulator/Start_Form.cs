a591b49614bfd2d7f09937a42efb6186bfe781cc
Xamfree44 <39438613+Xamfree44@users.noreply.github.com>
2018-05-19 17:51:49 +0300

    загружаю проект

---
+++ FireKeyboardSimulator/FireKeyboardSimulator/Start_Form.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FireKeyboardSimulator
+{
+    public partial class Start_Form : Form
+    {
+        Core1 Learn;
+        Core_2 SpeedUp;
+        Core_3 Records;
+        Core_4 Endless;
+        public Start_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                Core1 f = new Core1(this.textBox1.Text);
+                f.Show();
+            }
+        }
+    }
+}
