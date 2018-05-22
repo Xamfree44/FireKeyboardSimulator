bf5c609b5cdae165883f57ac670a47ac9c8f7aa0
Xamfree44 <39438613+Xamfree44@users.noreply.github.com>
2018-05-23 00:38:46 +0300

    update 3.1
    Сделал основную мехаНику. Первая форма наполовину функционирует

---
+++ FireKeyboardSimulator-master/FireKeyboardSimulator/FireKeyboardSimulator/Core2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FireKeyboardSimulator
+{
+    public partial class Core_2 : Form
+    {
+        
+        public Core_2(string data)
+        {
+            InitializeComponent();
+            this.data = data;
+            label1.Text = data;
+            
+        }
+        string data;
+        void CoreMechanics(int ctrl)
+        {
+
+            switch (ctrl)
+            {
+                case (1):
+
+                    break;
+                case (2): break;
+                case (3): break;
+                case (4): break;
+                case (5): break;
+            }
+
+        }
+    }
+}
